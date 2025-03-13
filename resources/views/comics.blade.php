b5c1ad8feb75858e538f57ce8c55ce0ac57b45d5
antoniorusso2 <russoantonio303@outlook.it>
2025-03-13 10:49:33 +0100

    move img folder from public to resources - display dynamic content for card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,14 +7,23 @@
 @section('content')
     @php
         // static comics data from assets folder
-        dd(config('comics'));
-        // $comics = config('comics');
+        // dd(config('comics'));
+        $comics = config('comics');
     @endphp
 
     <section class="all-comics">
         <div class="container">
-            <div class="row">
-
+            <div class="row gap-3">
+                @foreach ($comics as $comic)
+                    <div class="col-2">
+                        {{-- componente card con elemento comic --}}
+                        <x-card>
+                            <x-slot:thumb> {{ $comic['thumb'] }} </x-slot>
+                            <x-slot:title>{{ $comic['title'] }}</x-slot>
+                            <x-slot:description>{{ $comic['description'] }}</x-slot>
+                        </x-card>
+                    </div>
+                @endforeach
             </div>
         </div>
     </section>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,9 +1,7 @@
-<div class="card" style="width: 12rem">
-    <img src="" class=" card-img-top" alt="">
-    <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="card">
+    <img src="{{ $thumb }}" class="card-img-top object-fit-cover" alt="">
+    <div class="card-body ">
+        <h5 class="card-title">{{ $title }}</h5>
+        <p class="card-text text-truncate">{{ $description }}</p>
     </div>
 </div>
